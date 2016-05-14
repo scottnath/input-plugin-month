b98557f1fd3a9212399fc83f9c1f7fbbf287ded4
Sam Richard <sam@snug.ug>
2016-05-14 09:09:09 -0400

    :octocat::sparkles: Open Source :sparkles::octocat:

---
+++ README.md
@@ -0,0 +1,17 @@
+Month Input Plugin [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+---
+
+A simple single month input plugin
+
+## License
+
+Apache-2.0
+
+[npm-image]: https://badge.fury.io/js/input-plugin-month.svg
+[npm-url]: https://npmjs.org/package/input-plugin-month
+[travis-image]: https://travis-ci.org/punchcard-cms/input-plugin-month.svg
+[travis-url]: https://travis-ci.org/punchcard-cms/input-plugin-month
+[daviddm-image]: https://david-dm.org/punchcard-cms/input-plugin-month.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/punchcard-cms/input-plugin-month
+[coveralls-image]: https://coveralls.io/repos/punchcard-cms/input-plugin-month/badge.svg
+[coveralls-url]: https://coveralls.io/r/punchcard-cms/input-plugin-month
---
+++ index.js
@@ -0,0 +1,48 @@
+/**
+ * Month Input Plugin
+ *
+ *
+ * A simple single month input plugin
+ */
+const validation = require('./lib/validation.js');
+
+/**
+ * Single Month Input Plugin
+ * @module monthInputPlugin
+ */
+module.exports = {
+  name: 'Month',
+  description: 'A simple single month input plugin',
+  validation: {
+    monthValidation: validation,
+  },
+  inputs: {
+    month: {
+      validation: {
+        function: 'monthValidation',
+        on: 'change',
+      },
+      label: 'Choose a month',
+      type: 'select',
+      settings: {
+        empty: true,
+        size: '1',
+        months: {
+          '01': 'January',
+          '02': 'February',
+          '03': 'March',
+          '04': 'April',
+          '05': 'May',
+          '06': 'June',
+          '07': 'July',
+          '08': 'August',
+          '09': 'September',
+          '10': 'October',
+          '11': 'November',
+          '12': 'December',
+        },
+      },
+    },
+  },
+  html: '<label for="{{month.id}}">{{month.label}}</label><select id="{{month.id}}" name="{{month.name}}" size="{{month.size}}">{% for month, number in settings.months %}<option value="{{number}}">{{month}}</option>{% endfor %}</select>',
+};
