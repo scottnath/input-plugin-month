f22ce9c02a30d8ce718d37de69bd38441dde9d71
Scott Nath <snath@us.ibm.com>
2016-09-13 15:07:11 -0400

    :new: :fire: remove old required validation

--- tests/validation.js
+++ tests/validation.js
@@ -1,37 +1,7 @@
 import test from 'ava';
 import validation from '../lib/validation';
 
-const input = {
-  target: {
-    name: 'month',
-    value: 'foo bar baz',
-  },
-  all: {
-    month: 'foo bar baz',
-  },
-};
-
-const settings = {
-  target: {
-    empty: false,
-  },
-  all: {
-    month: {
-      empty: false,
-    },
-  },
-};
-
-
 // Valid input
 test('valid input', t => {
-  t.true(validation(input, settings), 'Valid input returns true');
-});
-
-// Invalid input
-test('validate correct input', t => {
-  const ip = input;
-  ip.target.value = '';
-
-  t.is(validation(ip, settings), 'month cannot be left blank!', 'Return string if not valid');
+  t.true(validation(), 'Empty input returns true');
 });
